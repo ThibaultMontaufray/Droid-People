a88851089ede1b06c7bdf29a3a7214f95fc8be42
Thibault MONTAUFRAY <amostrack@gmail.com>
2017-01-01 23:48:16 +0100

    First push

---
+++ Project/Controler/Interface_team.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Droid_Team
+{
+    public class Interface_team
+    {
+        #region Attribute
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructor
+        #endregion
+
+        #region Methods public
+
+        #region Action
+        [Description("french[ajouger.membre(membre)];english[add.member(member)]")]
+        public static void ACTION_130_add_member(Member member)
+        {
+
+        }
+        public static void ACTION_131_remove_member(Member member)
+        {
+
+        }
+        public static void ACTION_132_modify_member(Member member)
+        {
+
+        }
+        #endregion
+
+        #endregion
+
+        #region Methods private
+        #endregion
+    }
+}
